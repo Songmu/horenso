636d295e0bb7bf3c4d1060c541490fe96cb36f3c
Songmu <y.songmu@gmail.com>
2015-12-27 19:40:21 +0900

    testtimestampwriter

---
+++ timestampwriter_test.go
@@ -0,0 +1,21 @@
+package horenso
+
+import (
+	"bytes"
+	"regexp"
+	"testing"
+)
+
+func TestTimeStampWriter(t *testing.T) {
+	var bb bytes.Buffer
+	l := newTimestampWriter(&bb)
+	l.Write([]byte{'c'})
+	l.Write([]byte{'d'})
+	l.Write([]byte{'\n', 'e', 'f'})
+
+	timeRegStr := `\[[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{6}\] `
+	reg := regexp.MustCompile(`\A` + timeRegStr + "cd\n" + timeRegStr + `ef\z`)
+	if !reg.Match(bb.Bytes()) {
+		t.Errorf("something went wrong. output: %s", bb.String())
+	}
+}
